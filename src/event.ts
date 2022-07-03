d139d39aa8003fe0470e1418d4e903f2851f147c
cffh <cffhidol@163.com>
2022-07-03 18:41:06 +0800

    AsyncEvent实现

---
+++ src/event.ts
@@ -0,0 +1,33 @@
+class AsyncEvent {
+  listener: Map<string, Set<Function>> = new Map()
+  on(type: string, listener: Function) {
+    const { listener: ls } = this
+    const l = ls.get(type)
+    if (!l) ls.set(type, new Set([listener]))
+    else l.add(listener)
+  }
+  off(type: string, listener: Function) {
+    const { listener: ls } = this
+    const l = ls.get(type)
+    if (!l) return false
+    return l.delete(listener)
+  }
+  emit(type: string) {
+    return new Promise((e, r) => {
+      const ls = this.listener.get(type)
+      if (!ls) return
+      const als: Set<Promise<any>> = new Set()
+      ls.forEach((e) => {
+        const r = e()
+        if (r instanceof Promise) als.add(r)
+      })
+      Promise.all(ls)
+        .then((d) => {
+          e(d)
+        })
+        .catch((d) => {
+          r(d)
+        })
+    })
+  }
+}
